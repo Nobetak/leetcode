90cad7152e822bebbb04b76fb5c4024721c5af76
Nobetak <157054426+Nobetak@users.noreply.github.com>
2025-10-15 21:46:04 +0200

    Time: 6 ms (31.51%) | Memory: 12.6 MB (51.18%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -6,20 +6,20 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: float
         """   
-        med=0.0
-        tab=[]
-        tab=nums1
+        med=0.0 #tworze zmienna do zwrocenia wyniku
+        tab=[] #tworze tablice
+        tab=nums1 #dodaje do jednej tablicy 2 inne
         tab+=nums2
-        dlugosc=len(tab)
-        tab.sort()
-        if(dlugosc%2==0):
-            miejsce1=float(dlugosc)
-            miejsce1=int(math.ceil(dlugosc/2))
-            miejsce2=miejsce1-1
-            med=float(tab[miejsce1]+tab[miejsce2])/2
-            return med
+        dlugosc=len(tab) #zczytuje dlugosc poloczanych tablic
+        tab.sort() #funkcja sortujaca tablice
+        if(dlugosc%2==0): #jezeli dlugosc nie ma reszty tak jak w matmie
+            miejsce1=float(dlugosc) #zamieniam liczbe na zmiennoprzecinkowa
+            miejsce1=int(math.ceil(dlugosc/2)) # bo tutaj by mi wyszlo zle
+            miejsce2=miejsce1-1 # i odejmuje 1 od tego aby znac srodeklisty
+            med=float(tab[miejsce1]+tab[miejsce2])/2 # obliczan mediane
+            return med #zwracam mediane 
         else:
-            miejsce=float(dlugosc)
-            miejsce=int(math.ceil(dlugosc/2))
+            miejsce=float(dlugosc) #to samo tylko ze tutaj obliczam
+            miejsce=int(math.ceil(dlugosc/2)) #dla tablicy o wielkosc 3,5..
             med=float(tab[miejsce])
             return med
