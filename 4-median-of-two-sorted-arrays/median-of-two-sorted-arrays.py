db1f5923d3880edf0dcf3507df29b88879df0f1c
Nobetak <157054426+Nobetak@users.noreply.github.com>
2025-10-15 21:36:22 +0200

    Time: 2 ms (67.30%) | Memory: 12.7 MB (51.18%) - LeetSync

---
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -0,0 +1,25 @@
+import math
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """   
+        med=0.0
+        tab=[]
+        tab=nums1
+        tab+=nums2
+        dlugosc=len(tab)
+        tab.sort()
+        if(dlugosc%2==0):
+            miejsce1=float(dlugosc)
+            miejsce1=int(math.ceil(dlugosc/2))
+            miejsce2=miejsce1-1
+            med=float(tab[miejsce1]+tab[miejsce2])/2
+            return med
+        else:
+            miejsce=float(dlugosc)
+            miejsce=int(math.ceil(dlugosc/2))
+            med=float(tab[miejsce])
+            return med
