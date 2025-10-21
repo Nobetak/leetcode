0c88282562fb1a629a41555859f2f266f410316e
Nobetak <157054426+Nobetak@users.noreply.github.com>
2025-10-21 17:46:43 +0200

    Time: 351 ms (58.08%) | Memory: 12.6 MB (55.84%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,20 +1,28 @@
 class Solution(object):
     def longestPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        def czypalindrom(x):
-            odwroconyx=x[::-1]
-            if(x==odwroconyx):
-                return x
-            else: 
-                return ""
-        maksymalna=""
-        for i in  range(len(s)):
-            for j in range(i+1,len(s)+1):
-                zmienna=s[i:j]
-                if(zmienna==zmienna[::-1]):
-                    if(len(maksymalna)<len(zmienna)):
-                        maksymalna=zmienna
-        return maksymalna
+        if not s:
+            return ""
+
+        najdluzszy_palindrom = ""
+        
+        # Funkcja pomocnicza do rozszerzania od środka
+        def rozszerz_od_srodka(lewy, prawy):
+            while lewy >= 0 and prawy < len(s) and s[lewy] == s[prawy]:
+                lewy -= 1
+                prawy += 1
+            # Zwraca znaleziony palindrom
+            return s[lewy + 1:prawy]
+
+        # Iteracja przez każdy znak jako potencjalny środek
+        for i in range(len(s)):
+            # Palindrom o nieparzystej długości (środek to s[i])
+            palindrom_nieparzysty = rozszerz_od_srodka(i, i)
+            if len(palindrom_nieparzysty) > len(najdluzszy_palindrom):
+                najdluzszy_palindrom = palindrom_nieparzysty
+
+            # Palindrom o parzystej długości (środek między s[i] i s[i+1])
+            palindrom_parzysty = rozszerz_od_srodka(i, i + 1)
+            if len(palindrom_parzysty) > len(najdluzszy_palindrom):
+                najdluzszy_palindrom = palindrom_parzysty
+        
+        return najdluzszy_palindrom
